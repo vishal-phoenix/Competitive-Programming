8ad31b96c4df79a8e55c39b28138fd2d100484c3
Vishal <vishal29032002m@gmail.com>
2023-03-19 20:27:00 +0530

    Add solution for problem `A - Plus or Minus` Link: https://codeforces.com/contest/1807/problem/A Tags: implementation Ref: https://codeforces.com/contest/1807/submission/198114677

---
+++ codeforces/1807/A.cpp
@@ -0,0 +1,37 @@
+// Code by Vishal
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define ff first
+#define ss second
+#define endl "\n"
+#define all(x) (x).begin(),(x).end()
+#define scanit(a,n) for(ll indexaa=0;indexaa<n;indexaa++) cin>>a[indexaa];
+#define printit(a,n) for(ll indexaa=0; indexaa<n; indexaa++) cout<<a[indexaa]<<" "; cout<<endl;
+#define pb push_back
+inline ll power(ll x, unsigned ll y, ll p = LLONG_MAX) {ll res=1; x=x%p; if(x==0) {return 0;} while(y>0){ if(y&1){res=(res*x)%p;} y=y>>1; x=(x*x)%p;} return res;} // CALCULATING POWER IN LOG(Y) TIME COMPLEXITY
+#define testcase ll t; cin>>t; while(t--)
+#define flash  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout<<fixed; cout<<setprecision(10);
+using namespace std;
+
+
+int main()
+{
+    flash;
+    testcase{
+        ll a,b,c;
+        cin>>a>>b>>c;
+        if(c==(a+b)){
+            cout<<"+"<<endl;
+        }else{
+            cout<<"-"<<endl;
+        }
+        
+
+    }
+
+
+   
+    
+    return 0;
+}
