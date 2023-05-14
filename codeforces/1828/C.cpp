0a696a34fcac3ca09a0833794e1626a4929ee766
Vishal <vishal29032002m@gmail.com>
2023-05-14 21:31:00 +0530

    Add solution for problem `C - Counting Orders` Link: https://codeforces.com/contest/1828/problem/C Tags: binary search, combinatorics, sortings, *1100 Ref: https://codeforces.com/contest/1828/submission/205891741

---
+++ codeforces/1828/C.cpp
@@ -0,0 +1,44 @@
+// Code by Vishal
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define ff first
+#define ss second
+#define endl "\n"
+#define all(x) (x).begin(),(x).end()
+#define timetaken cerr<<fixed<<setprecision(10); cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl
+#define scanit(a,n) for(ll indexaa=0;indexaa<n;indexaa++) cin>>a[indexaa];
+#define printit(a,n) for(ll indexaa=0; indexaa<n; indexaa++) cout<<a[indexaa]<<" "; cout<<endl;
+#define pb push_back
+inline ll power(ll x, unsigned ll y, ll p = LLONG_MAX) {ll res=1; x=x%p; if(x==0) {return 0;} while(y>0){ if(y&1){res=(res*x)%p;} y=y>>1; x=(x*x)%p;} return res;} // CALCULATING POWER IN LOG(Y) TIME COMPLEXITY
+#define testcase ll t; cin>>t; while(t--)
+#define flash  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout<<fixed; cout<<setprecision(10);
+using namespace std;
+ll mod = 1000000007;
+
+int main()
+{
+    flash;
+    testcase{
+        ll n;
+        cin>>n;
+        vector<ll> a(n);
+        scanit(a,n);
+        vector<ll> b(n);
+        scanit(b,n);
+        sort(all(b));
+        sort(all(a));
+        ll ans = 1;
+        for(ll i=0;i<n;i++){
+            ll x = upper_bound(all(a),b[n-1-i])-a.begin();
+            if(a[x]<=b[n-1-i]){
+                ans = 0;
+                break;
+            }
+            ans = ((ans%mod)*(n-x-i)%mod)%mod;
+        }
+        cout<<ans<<endl;
+    }
+    timetaken;
+    return 0;
+}
